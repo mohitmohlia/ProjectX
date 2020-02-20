d58945a72ae217bd2cf2336b5611bfd0d79d8c40
Mohit Mohlia <q3technologies@hotmail.com>
2020-02-20 11:01:48 +0530

    Data Binding lesson 20th Feb 2020

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector:'app-server',
    templateUrl:'./server.component.html'
-    //template:'<app-server></app-server>'
+   
 })
 export class ServerComponent{
+ServerId:number=10;
+ServerStatus: string ='Offline';
+getServerStatus(){
+    return this.ServerStatus;
+}
 
 }
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  template: `
+    <p> This is the warning message </p>
+  `,
+  styles: [
+  `
+      p{
+        padding : 20px;
+        background-color: green;
+        border: 8px solid red;
+      }
+  `  
+  ]
 })
 export class WarningAlertComponent implements OnInit {
 
